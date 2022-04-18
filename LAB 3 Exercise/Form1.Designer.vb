af4d02f398d485f7deee24310ec9716615287361
Dell <Dell@DESKTOP-UCNTS0G>
2022-04-18 17:16:06 +0800

    Add project files.

---
+++ LAB 3 Exercise/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.weightListBox = New System.Windows.Forms.ListBox()
+        Me.zoneListBox = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(120, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Weight"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(120, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "[Not to exceed]"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(379, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Zone"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(273, 331)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Changes :"
+        '
+        'weightListBox
+        '
+        Me.weightListBox.FormattingEnabled = True
+        Me.weightListBox.ItemHeight = 20
+        Me.weightListBox.Items.AddRange(New Object() {"1 lb", "3 lb", "5 lb", "10 lb", ">10 lb"})
+        Me.weightListBox.Location = New System.Drawing.Point(120, 197)
+        Me.weightListBox.Name = "weightListBox"
+        Me.weightListBox.Size = New System.Drawing.Size(150, 104)
+        Me.weightListBox.TabIndex = 4
+        '
+        'zoneListBox
+        '
+        Me.zoneListBox.FormattingEnabled = True
+        Me.zoneListBox.ItemHeight = 20
+        Me.zoneListBox.Items.AddRange(New Object() {"A", "B", "C", "D"})
+        Me.zoneListBox.Location = New System.Drawing.Point(362, 197)
+        Me.zoneListBox.Name = "zoneListBox"
+        Me.zoneListBox.Size = New System.Drawing.Size(150, 104)
+        Me.zoneListBox.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(362, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Look Up"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TextBox1.Location = New System.Drawing.Point(362, 331)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(120, 30)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.zoneListBox)
+        Me.Controls.Add(Me.weightListBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents weightListBox As ListBox
+    Friend WithEvents zoneListBox As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As Label
+End Class
---
+++ LAB 3 Exercise/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A1(4, 3) As String
+        A1(0, 0) = "1.00"
+        A1(0, 1) = "1.50"
+        A1(0, 2) = "1.65"
+        A1(0, 3) = "1.85"
+        A1(1, 0) = "1.58"
+        A1(1, 1) = "2.00"
+        A1(1, 2) = "2.40"
+        A1(1, 3) = "3.05"
+        A1(2, 0) = "1.71"
+        A1(2, 1) = "2.52"
+        A1(2, 2) = "3.10"
+        A1(2, 3) = "4.00"
+        A1(3, 0) = "2.04"
+        A1(3, 1) = "3.12"
+        A1(3, 2) = "4.00"
+        A1(3, 3) = "5.01"
+        A1(4, 0) = "2.52"
+        A1(4, 1) = "3.75"
+        A1(4, 2) = "5.10"
+        A1(4, 3) = "7.25"
+
+        TextBox1.Text = A1(weightListBox.SelectedIndex, zoneListBox.SelectedIndex)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+
+    End Sub
+End Class
